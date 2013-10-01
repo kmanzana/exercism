2323bc69ce834d97827d21f008343b2ac329e0ff
Kelton Manzanares <kmanzana@mines.edu>
2013-09-30 19:00:20 -0600

    another submission for ruby

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,37 +1,34 @@
 class PhoneNumber
   attr_reader :number
   
-  def initialize(number)
-    @number = number
-    sanitize_number!
+  def initialize(dirty_number)
+    @number = sanitize(dirty_number)
   end
 
   def area_code
-    @area_code ||= number[0..2]
+    number[0..2]
   end
 
   def to_s
-    @to_s ||= "(#{area_code}) #{exchange}-#{station}"
+    "(#{area_code}) #{exchange}-#{station}"
   end
 
   private
 
-  def sanitize_number!
-    get_digits!
-    fix_number!
+  def sanitize(dirty_number)
+    @number = get_digits(dirty_number)
+    validate_number
   end
 
-  def get_digits!
-    # @number = number.delete("^0-9")
-    # @number = number.gsub(/\D/, '')
-    @number = number.split(/\D/).join
+  def get_digits(dirty_number)
+    dirty_number.delete("^0-9")
   end
 
-  def fix_number!
-    @number = if number.length == 11 && number.start_with?('1')
-      remove_1
-    elsif number.length == 10
+  def validate_number
+    if number.length == 10
       number
+    elsif number.length == 11 && number.start_with?('1')
+      remove_country_code
     else
       bad_number
     end
@@ -41,8 +38,8 @@ class PhoneNumber
     '0' * 10
   end
 
-  def remove_1
-    @number[1..-1]
+  def remove_country_code
+    number[1..-1]
   end
 
   def exchange
